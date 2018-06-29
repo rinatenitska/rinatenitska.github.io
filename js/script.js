65592cb4324a92d6a1e823b134821e1f51fa7efc
rina-tenitska <39187065+rina-tenitska@users.noreply.github.com>
2018-06-29 11:12:21 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by EveRin on 29.06.2018.
  */
-var weatherHeight = $("#parameters").height();
+/*var weatherHeight = $("#parameters").height();
 $("#current-weather").css("height", weatherHeight);
 $(window).on("resize", function () {
     var weatherHeight = $("#parameters").height();
@@ -12,4 +12,4 @@ $("#temp").css("top", itemHeight);
 $(window).on("resize", function () {
     var itemHeight = $("#current-weather").height()/2 - $("#temp").height()/2;
     $("#temp").css("top", itemHeight);
-});
+});*/
